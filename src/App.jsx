21ef85f4a78c6a5294ea102d2f69cb221cb17c25
Stefan Hancar <stefan@hancar.sk>
2023-08-18 11:07:33 +0200

    add favorites,search

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
 import { useState } from "react";
+import {
+  Tabs,
+  Input,
+  Flex,
+  TabList,
+  TabPanel,
+  Tab,
+  TabPanels,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 
 import NavigationBar from "./components/NavigationBar";
 
 function App() {
-  return <NavigationBar />;
+  return (
+    <>
+      <NavigationBar />
+      <Tabs isFitted variant="enclosed">
+        <TabList>
+          <Tab>Favorites</Tab>
+          <Tab>Search</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <p>favorites</p>
+          </TabPanel>
+          <TabPanel>
+            <Flex mx={"auto"} mt={3} gap={4} w={"min(720px,90%)"}>
+              <InputGroup>
+                <InputLeftElement
+                  pointerEvents="none"
+                  height={"full"}
+                  children={<SearchIcon />}
+                />
+                <Input size={"lg"} />
+              </InputGroup>
+            </Flex>
+            <p>search</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </>
+  );
 }
 
 export default App;
